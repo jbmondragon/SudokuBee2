3d1b7cdc17e49a8751e5b8f8f3ccb3fdc7528728
Kenneth Montes <kamontes@up.edu.ph>
2025-10-24 02:24:23 +0800

    added number on pop-up menu(still needs fixing)

--- src/UIPop.java
+++ src/UIPop.java
@@ -16,22 +16,57 @@ public class UIPop extends generalPanel {
 		this.pane = pane;
 		this.size = size;
 		panel = addPanel(pane, 5, 84, 500, 500);
-		panel.setOpaque(true);
+		panel.setOpaque(false);
+
+		bg = addLabel(panel, "img/game control/" + size + "x" + size + ".png", 0, 0);
+		javax.swing.Icon bgIcon = bg.getIcon();
+		int bgW = bgIcon.getIconWidth();
+		int bgH = bgIcon.getIconHeight();
 
 		// Create buttons dynamically
 		btn = new JButton[size];
 		int gridCols = (int) Math.ceil(Math.sqrt(size)); // number of columns
 		int gridRows = (int) Math.ceil((double) size / gridCols); // number of rows
-		int startX = 50, startY = 150; // base coordinates
-		int spacingX = 60, spacingY = 60; // button spacing
+		String sampleIconPath = "img/box/" + size + "x" + size + "/normal/1.png";
+		int btnW = 48, btnH = 48;
+		java.io.File sampleFile = new java.io.File(sampleIconPath);
+		if (sampleFile.exists()) {
+			javax.swing.Icon sampleIcon = new javax.swing.ImageIcon(sampleIconPath);
+			btnW = sampleIcon.getIconWidth();
+			btnH = sampleIcon.getIconHeight();
+		}
+		int gap = 8;
+		int spacingX = btnW + gap, spacingY = btnH + gap;
+		int startX = (bgW - (gridCols * btnW + (gridCols - 1) * gap)) / 2;
+		int startY = (bgH - (gridRows * btnH + (gridRows - 1) * gap)) / 2;
 
 		for (int i = 0; i < size; i++) {
 			int row = i / gridCols;
 			int col = i % gridCols;
-			btn[i] = addButton(panel,
-					"img/box/12x12/normal/" + (i + 1) + ".png",
-					startX + col * spacingX,
-					startY + row * spacingY);
+			String btnFolder = "img/box/" + size + "x" + size + "/normal/";
+			String iconPath = btnFolder + (i + 1) + ".png";
+			java.io.File f = new java.io.File(iconPath);
+			int bx = startX + col * spacingX;
+			int by = startY + row * spacingY;
+			if (f.exists() && f.length() > 0) {
+				btn[i] = addButton(panel, iconPath, bx, by);
+			} else {
+				btn[i] = new JButton((i + 1) + "");
+				btn[i].setBounds(bx, by, 48, 48);
+				btn[i].setOpaque(true);
+				btn[i].setContentAreaFilled(true);
+				btn[i].setBorderPainted(true);
+				panel.add(btn[i]);
+			}
+		}
+
+		try {
+			for (int i = 0; i < size; i++) {
+				if (btn[i] != null)
+					panel.setComponentZOrder(btn[i], 0);
+			}
+			panel.setComponentZOrder(bg, panel.getComponentCount() - 1);
+		} catch (Exception e) {
 		}
 
 		erase = addButton(panel, "img/box/misc/clear.png", 50, 50);
@@ -39,8 +74,6 @@ public class UIPop extends generalPanel {
 
 		field = addTextField(panel, "", 200, 100, 80, 38);
 		field.grabFocus();
-
-		bg = addLabel(panel, "img/game control/" + size + "x" + size + ".png", 0, 0);
 	}
 
 	protected void setVisible(boolean isVisible, int btnX, int btnY, int num) {
