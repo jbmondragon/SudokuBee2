e106664d64bd8c2b8f5c219927f09ee2544ab20e
Jake Mondragon <mondragonjake53@gmail.com>
2025-10-27 14:26:00 +0800

    Fixed Position of Buttons for UIPOP

--- src/UIPop.java
+++ src/UIPop.java
@@ -15,39 +15,36 @@ public class UIPop extends generalPanel {
 	UIPop(int size, JPanel pane) {
 		this.pane = pane;
 		this.size = size;
+
+		// Main panel
 		panel = addPanel(pane, 5, 84, 500, 500);
 		panel.setOpaque(false);
 
+		// Background label
 		bg = addLabel(panel, "img/game control/" + size + "x" + size + ".png", 0, 0);
 		javax.swing.Icon bgIcon = bg.getIcon();
 		int bgW = bgIcon.getIconWidth();
 		int bgH = bgIcon.getIconHeight();
 
-		// Create buttons dynamically
-		btn = new JButton[size];
-		int gridCols = (int) Math.ceil(Math.sqrt(size)); // number of columns
-		int gridRows = (int) Math.ceil((double) size / gridCols); // number of rows
-		String sampleIconPath = "img/box/" + size + "x" + size + "/normal/1.png";
-		int btnW = 48, btnH = 48;
-		java.io.File sampleFile = new java.io.File(sampleIconPath);
-		if (sampleFile.exists()) {
-			javax.swing.Icon sampleIcon = new javax.swing.ImageIcon(sampleIconPath);
-			btnW = sampleIcon.getIconWidth();
-			btnH = sampleIcon.getIconHeight();
-		}
-		int gap = 8;
-		int spacingX = btnW + gap, spacingY = btnH + gap;
-		int startX = (bgW - (gridCols * btnW + (gridCols - 1) * gap)) / 2;
-		int startY = (bgH - (gridRows * btnH + (gridRows - 1) * gap)) / 2;
+		// Send background to back
+		panel.setComponentZOrder(bg, panel.getComponentCount() - 1);
 
+		// Hardcoded positions for 9 main buttons
+		int[][] positions = {
+				{ 100, 180 }, { 182, 180 }, { 255, 180 },
+				{ 330, 180 }, { 150, 250 }, { 220, 250 },
+				{ 295, 250 }, { 180, 315 }, { 257, 315 }
+		};
+
+		// Create main buttons
+		btn = new JButton[size];
 		for (int i = 0; i < size; i++) {
-			int row = i / gridCols;
-			int col = i % gridCols;
-			String btnFolder = "img/box/" + size + "x" + size + "/normal/";
-			String iconPath = btnFolder + (i + 1) + ".png";
+			int bx = positions[i][0];
+			int by = positions[i][1];
+
+			String iconPath = "img/box/" + size + "x" + size + "/normal/" + (i + 1) + ".png";
 			java.io.File f = new java.io.File(iconPath);
-			int bx = startX + col * spacingX;
-			int by = startY + row * spacingY;
+
 			if (f.exists() && f.length() > 0) {
 				btn[i] = addButton(panel, iconPath, bx, by);
 			} else {
@@ -58,20 +55,23 @@ public class UIPop extends generalPanel {
 				btn[i].setBorderPainted(true);
 				panel.add(btn[i]);
 			}
+
+			panel.setComponentZOrder(btn[i], 0);
 		}
 
-		try {
-			for (int i = 0; i < size; i++) {
-				if (btn[i] != null)
-					panel.setComponentZOrder(btn[i], 0);
-			}
-			panel.setComponentZOrder(bg, panel.getComponentCount() - 1);
-		} catch (Exception e) {
-		}
+		// Top-layer buttons (erase and cancel) - positions unchanged
+		erase = addButton(panel, "img/box/misc/clear.png", 143, 125);
+		cancel = addButton(panel, "img/box/misc/cancel.png", 286, 125);
 
-		erase = addButton(panel, "img/box/misc/clear.png", 50, 50);
-		cancel = addButton(panel, "img/box/misc/cancel.png", 150, 50);
+		panel.setComponentZOrder(erase, 0);
+		panel.setComponentZOrder(cancel, 0);
 
+		erase.setOpaque(false);
+		erase.setContentAreaFilled(false);
+		cancel.setOpaque(false);
+		cancel.setContentAreaFilled(false);
+
+		// Text field
 		field = addTextField(panel, "", 200, 100, 80, 38);
 		field.grabFocus();
 	}
